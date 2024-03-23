62c46265a146a0c34dbab08e6b52d30fed88146b
Jon Zudell <jonathon.zudell@gmail.com>
2024-03-23 11:39:04 -0500

    Ladder shutdown

--- ladder.go
+++ ladder.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"context"
+	"log"
+)
+
 type Ladder struct {
 	clients    map[*Client]bool
 	broadcast  chan []byte
@@ -20,13 +25,16 @@ func (l *Ladder) run() {
 	for {
 		select {
 		case client := <-l.register:
+			log.Println("Registering Client")
 			l.clients[client] = true
 		case client := <-l.unregister:
+			log.Println("Unregistering Client")
 			if _, ok := l.clients[client]; ok {
 				delete(l.clients, client)
 				close(client.send)
 			}
 		case message := <-l.broadcast:
+			log.Println("Broadcasting Message to all Clients")
 			for client := range l.clients {
 				select {
 				case client.send <- message:
@@ -36,5 +44,12 @@ func (l *Ladder) run() {
 				}
 			}
 		}
+	}
+}
+func (l *Ladder) Shutdown(ctx context.Context) {
+	log.Println("Shutdown Ladder")
+	for client := range l.clients {
+		close(client.send)
+		delete(l.clients, client)
 	}
 }
--- main.go
+++ main.go
@@ -23,33 +23,8 @@ func index(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
-// func upgrade(w http.ResponseWriter, r *http.Request) {
-// 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-// 	// upgrade this connection to a WebSocket connection
-// 	ws, err := upgrader.Upgrade(w, r, nil)
-// 	if err != nil {
-// 		log.Println("upgrade error %s", err)
-// 		return
-// 	}
-// 	for {
-// 		// Read message from browser
-// 		msgType, msg, err := ws.ReadMessage()
-// 		if err != nil {
-// 			return
-// 		}
-
-// 		// Print the message to the console
-// 		fmt.Printf("%s sent: %s\n", ws.RemoteAddr(), string(msg))
-
-// 		// Write message back to browser
-// 		if err = ws.WriteMessage(msgType, msg); err != nil {
-// 			return
-// 		}
-// 	}
-// }
-
 func main() {
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	ctx := context.Background()
 
@@ -73,6 +48,7 @@ func main() {
 	go func() {
 		<-c
 		log.Println("Caught SIGINT")
+		ladder.Shutdown(ctx)
 		server.Shutdown(ctx)
 	}()
 
@@ -80,7 +56,7 @@ func main() {
 
 	err := server.ListenAndServe()
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Println("ListenAndServe: ", err)
 	}
 	log.Println("Clean Shutdown")
 }
